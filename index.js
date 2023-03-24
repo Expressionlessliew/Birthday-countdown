a40dc9d46fca25e75dfb0c44109e4eec7f07c77a
Expressionlessliew <xinyi.liew09@gmail.com>
2023-03-24 22:06:55 +0800

    hello

---
+++ index.js
@@ -0,0 +1,14 @@
+const Days = document.getElementById("day");
+const Week = document.getElementById("week");
+const Savebtn = document.getElementById("save");
+let NewData;
+
+Days.addEventListener("select", handleChange);
+Week.addEventListener("select", handleChange);
+Savebtn.addEventListener("click",saveData)
+
+function saveData(){
+
+}
+
+console.log(Savebtn)
